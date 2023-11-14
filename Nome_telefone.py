dff4ddb70a38d82252672c011531dbeb23e11e82
Henrique Damasceno <124740131+RiquHen@users.noreply.github.com>
2023-11-13 23:50:46 -0300

    correções

--- Nome_telefone.py
+++ Nome_telefone.py
@@ -2,36 +2,52 @@
 # cadastro, e crie um arquivo com essas informacoes, uma por linha. O usuario finaliza a
 # entrada com ‘0’ para o telefone.
 import os, sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from ui_agenda import Ui_Agenda
 
 
 nomes = []
 numeros = []
-email =[]
+emails =[]
 class Agenda(QMainWindow, Ui_Agenda):
-    
-    with open('agenda.txt', 'a', encoding='utf8') as a:
-        while True:
-            nome = str(input("Nome(0 - SAIR): "))
+    def __init__(self):
+        super(Agenda,self).__init__()
+        self.setupUi(self)
+        
+        self.btn_salvar.clicked.connect(self.cadastro)
+        
+    def cadastro(self):
+        with open('agenda.txt', 'a', encoding='utf8') as a:
+            nome = self.ln_contato.text()
             if nome.isnumeric():
-                if int(nome) == 0:
-                    print("Fechando a Agenda!!! Obrigado!")
-                    break    
-            fone = str(input("Telefone: "))     
+                QMessageBox.warning(QMessageBox(), 'ERRO','Nome Inválido!!!!')
+            fone = self.ln_fone.text()
+            if not fone.isnumeric():
+                QMessageBox.warning(QMessageBox(), 'ERRO', "Número digitado inválido.")
+            email = self.ln_email.text()   
             if not nome.isnumeric() and fone.isnumeric():
                 nomes.append(nome)
                 numeros.append(fone)
-            if not fone.isnumeric():
-                print("Número digitado inválido.")
+                emails.append(email)
+            self.limpar()
+                    
                 
+            for i in range(len(nomes)):
+                a.write(f'\n{nomes[i]:<15}{numeros[i]:>11}')
+
+        """with open('agenda.txt', 'r', encoding='utf8') as a:
+            print(a.read())"""
+
+    def limpar(self):
+        self.ln_contato.clear()
+        self.ln_email.clear()
+        self.ln_fone.clear()
+        self.ln_contato.setFocus()
             
-        for i in range(len(nomes)):
-            a.write(f'\n{nomes[i]:<15}{numeros[i]:>11}')
-
-    with open('agenda.txt', 'r', encoding='utf8') as a:
-        print(a.read())
-
+app = QApplication(sys.argv)
+w=Agenda()
+w.show()
+app.exit(app.exec())
 
 
 
